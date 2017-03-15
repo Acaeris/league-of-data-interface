1ccf9de2fb5272e59765f556b2bbb4f01557cf00
Acaeris <acaeris@gmail.com>
2017-03-15 15:28:39 +0000

    Code Quality improvements to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,8 @@
 // Karma configuration
 // Generated on Wed Jul 15 2015 09:44:02 GMT+0200 (Romance Daylight Time)
-'use strict';
+"use strict";
 
-var argv = require('yargs').argv;
+var argv = require("yargs").argv;
 var minimatch = require("minimatch");
 
 
@@ -10,64 +10,64 @@ module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './',
+    basePath: "./",
 
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ["jasmine"],
 
 
     // list of files / patterns to load in the browser
     files: [
       // Polyfills.
-      'node_modules/core-js/client/shim.min.js',
-      'node_modules/intl/dist/Intl.min.js',
+      "node_modules/core-js/client/shim.min.js",
+      "node_modules/intl/dist/Intl.min.js",
 
-      'node_modules/traceur/bin/traceur.js',
+      "node_modules/traceur/bin/traceur.js",
 
       // System.js for module loading
-      'node_modules/systemjs/dist/system.src.js',
+      "node_modules/systemjs/dist/system.src.js",
 
       // Zone.js dependencies
-      'node_modules/zone.js/dist/zone.js',
-      'node_modules/zone.js/dist/long-stack-trace-zone.js',
-      'node_modules/zone.js/dist/async-test.js',
-      'node_modules/zone.js/dist/fake-async-test.js',
-      'node_modules/zone.js/dist/sync-test.js',
-      'node_modules/zone.js/dist/proxy.js',
-      'node_modules/zone.js/dist/jasmine-patch.js',
+      "node_modules/zone.js/dist/zone.js",
+      "node_modules/zone.js/dist/long-stack-trace-zone.js",
+      "node_modules/zone.js/dist/async-test.js",
+      "node_modules/zone.js/dist/fake-async-test.js",
+      "node_modules/zone.js/dist/sync-test.js",
+      "node_modules/zone.js/dist/proxy.js",
+      "node_modules/zone.js/dist/jasmine-patch.js",
 
       // RxJs.
-      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
+      { pattern: "node_modules/rxjs/**/*.js", included: false, watched: false },
+      { pattern: "node_modules/rxjs/**/*.js.map", included: false, watched: false },
 
       // paths loaded via module imports
       // Angular itself
-      { pattern: 'node_modules/@angular/**/*.js', included: false, watched: true },
-      { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
+      { pattern: "node_modules/@angular/**/*.js", included: false, watched: true },
+      { pattern: "node_modules/@angular/**/*.js.map", included: false, watched: false },
 
-      { pattern: 'dist/dev/**/*.js', included: false, watched: true },
-      { pattern: 'dist/dev/**/*.html', included: false, watched: true, served: true },
-      { pattern: 'dist/dev/**/*.css', included: false, watched: true, served: true },
-      { pattern: 'node_modules/systemjs/dist/system-polyfills.js', included: false, watched: false }, // PhantomJS2 (and possibly others) might require it
+      { pattern: "dist/dev/**/*.js", included: false, watched: true },
+      { pattern: "dist/dev/**/*.html", included: false, watched: true, served: true },
+      { pattern: "dist/dev/**/*.css", included: false, watched: true, served: true },
+      { pattern: "node_modules/systemjs/dist/system-polyfills.js", included: false, watched: false }, // PhantomJS2 (and possibly others) might require it
 
       // suppress annoying 404 warnings for resources, images, etc.
-      { pattern: 'dist/dev/assets/**/*', watched: false, included: false, served: true },
+      { pattern: "dist/dev/assets/**/*", watched: false, included: false, served: true },
 
-      'test-config.js',
-      'dist/dev/app/system-config.js',
-      'test-main.js'
+      "test-config.js",
+      "dist/dev/app/system-config.js",
+      "test-main.js"
     ],
 
     // must go along with above, suppress annoying 404 warnings.
     proxies: {
-      '/assets/': '/base/dist/dev/assets/'
+      "/assets/": "/base/dist/dev/assets/"
     },
 
     // list of files to exclude
     exclude: [
-      'node_modules/**/*spec.js'
+      "node_modules/**/*spec.js"
     ],
 
 
@@ -77,7 +77,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ["mocha"],
 
 
     // web server port
@@ -100,14 +100,14 @@ module.exports = function (config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-      'Chrome'
+      "Chrome"
     ],
 
 
     customLaunchers: {
       Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
+        base: "Chrome",
+        flags: ["--no-sandbox"]
       }
     },
 
@@ -122,13 +122,13 @@ module.exports = function (config) {
   });
 
   if (process.env.APPVEYOR) {
-    config.browsers = ['IE'];
+    config.browsers = ["IE"];
     config.singleRun = true;
     config.browserNoActivityTimeout = 90000; // Note: default value (10000) is not enough
   }
 
   if (process.env.TRAVIS || process.env.CIRCLECI) {
-    config.browsers = ['Chrome_travis_ci'];
+    config.browsers = ["Chrome_travis_ci"];
     config.singleRun = true;
     config.browserNoActivityTimeout = 90000;
   }
